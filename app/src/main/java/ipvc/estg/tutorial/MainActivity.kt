71b33083ee8ef20057445dfd8ab5aacfb510b31e
pereirajoaoEI <61158154+pereirajoaoEI@users.noreply.github.com>
2021-03-07 22:38:34 +0000

    Aplicação dos Ciclos de Vida

--- app/src/main/java/ipvc/estg/tutorial/MainActivity.kt
+++ app/src/main/java/ipvc/estg/tutorial/MainActivity.kt
@@ -2,10 +2,37 @@ package ipvc.estg.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("testeLog", "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("testeLog", "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("testeLog", "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("testeLog", "onResume")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("testeLog", "onDestroy")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("testeLog", "onRestart")
     }
 }
